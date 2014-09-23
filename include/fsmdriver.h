e4ac6577abcb7b0a21ecf16b46146a68ae1f2e2a
Bruno Macedo <120079585@aluno.unb.br>
2014-09-23 17:34:24 -0300

    Added setter and getter methods, fixed interface

--- include/fsmdriver.h
+++ include/fsmdriver.h
@@ -1,20 +1,107 @@
 #ifndef FSMDRIVER_H
 #define FSMDRIVER_H
 
-#include "state.h"
+#define PID_DT 0.02
+  /** @todo verificar se este valor está correto. */
+
+#define FINAL_SPEED 300 /** @todo Definir corretamente */
+#define KP 0.1   /** @todo Definir corretamente */
+#define KI 0.01  /** @todo Definir corretamente */
+#define KD 0.005 /** @todo Definir corretamente */
+
+#include "PID.h"
+#include "SimpleParser.h"
+#include "WrapperBaseDriver.h"
+
+using namespace std;
 
 //-----------------------------------------------------------------------------------------------------------------------
+//Class Prototypes
+class FsmDriver;
+class State;
+
+//-----------------------------------------------------------------------------------------------------------------------
+//Abstract class for modeling behavior according to it's current state.
+
+class State {
+public:
+    //Virtual method to drive according the state
+    virtual CarControl execute(FsmDriver* fsmdriver)  = 0;
+
+    //Virtual destructor
+    virtual ~State(){}
+};
+
+//------------------------------------------------------------------------------------------------------------------------
+//Curve State
+
+class StateCurve : public State {
+public:
+    virtual CarControl execute(FsmDriver *fsmdriver);
+    StateCurve(){}
+    ~StateCurve(){}
+};
+
+//------------------------------------------------------------------------------------------------------------------------
+//Straight Line State
+
+class StateStraightLine : public State {
+public:
+    virtual CarControl execute(FsmDriver *fsmdriver);
+    StateStraightLine();
+    ~StateStraightLine(){}
+private:
+	float finalSpeed, desiredDirection;	/** Target Speed **/
+    PIDController speedPID;
+	
+	int getGear(CarState & cs);
+	float getSteering(CarState & cs);
+	float getDistTrackAxis(CarState & cs);
+	float getSpeed(CarState & cs);
+};
+
+//------------------------------------------------------------------------------------------------------------------------
+//Out of the Track State
+
+class StateOutOfTrack : public State {
+public:
+    virtual CarControl execute(FsmDriver *fsmdriver);
+    ~StateOutOfTrack(){}
+    StateOutOfTrack(){}
+};
+
+//------------------------------------------------------------------------------------------------------------------------
 //Finite State Machine Driver
 
 class FsmDriver : public WrapperBaseDriver {
 private:
-    State* _state;
+    State* _state;      //Pointer to current state
+    CarState _cs;       //Sensorial information
 public:
-    FsmDriver();
-    void ChangeState(State* _newState);
+    // Print a shutdown message
+    virtual void onShutdown();
+
+    // Print a restart message
+    virtual void onRestart();
+
+    // Initialization of the desired angles for the rangefinders
+    virtual void init(float *angles);
+
+    //Empty constructor and destructor
+    FsmDriver(){}
+    ~FsmDriver(){}
+
+    //Getters and Setters methods.
+    CarState &getCarState();
+    void setCarState(CarState);
+    void SetState(State* _newState);
+    State* getState();
+
+    //Checks whenever _state need to be changed.
     State* transition(CarState&);
-    string drive(string);
+
+    //Main driving function.
+    virtual CarControl wDrive(CarState cs);
 };
 
-//-----------------------------------------------------------------------------------------------------------------------
 #endif // FSMDRIVER_H
--- src/fsmdriver.cpp
+++ src/fsmdriver.cpp
@@ -1,19 +1,40 @@
 #include "fsmdriver.h"
 
 //-------------------------------------------------------------------------------------------------------------------
-FsmDriver::FsmDriver() {
-
+CarState& FsmDriver::getCarState() {
+    return this->_cs;
 }
 
-void FsmDriver::ChangeState(State *_newState) {
-    delete _state;
-    _state = _newState;
+void FsmDriver::setCarState(CarState cs) {
+    this->_cs = cs;
 }
 
-string FsmDriver::drive(string sensors) {
-    CarState cs(sensors);
-    this->ChangeState(transition(cs));
-    return (_state->execute(this, cs));
+State* FsmDriver::getState() {
+    return this->_state;
+}
+
+void FsmDriver::SetState(State *_newState) {
+    delete this->_state;
+    this->_state = _newState;
+}
+
+CarControl FsmDriver::wDrive(CarState cs) {
+    this->setCarState(cs);
+    this->SetState(transition(this->getCarState()));
+    return (this->getState())->execute(this);
+}
+
+void FsmDriver::onRestart() {
+    cout << "Restarting the race!" << endl;
+}
+
+void FsmDriver::onShutdown() {
+    cout << "End of race!" << endl;
+}
+
+void FsmDriver::init(float *angles){
+    for (int i = 0; i < 19; ++i)
+        angles[i]=-90+i*10;
 }
 
 State* FsmDriver::transition(CarState &cs) {
@@ -25,4 +46,110 @@ State* FsmDriver::transition(CarState &cs) {
      *
      * */
 }
-//--------------------------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------------------
+//StateCurve Class
+CarControl StateCurve::execute(FsmDriver *fsmdriver) {
+    /*
+     *
+     *
+     * INSERT CRESTANI'S CODE HERE.
+     *
+     *
+     * */
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+//StateStraigtLine Class
+
+int StateStraightLine::getGear(CarState & cs) {
+    int current_gear = cs.getGear();
+    if(!current_gear) return 1;
+
+    // if(current_gear == 1 || current_gear == 2)   std::cout << "Current Gear = " << current_gear <<  " RPM = " << cs.getRpm() << std::endl;
+
+    if(current_gear > 1 && current_gear < 4 && cs.getRpm() < 1500)
+        return(current_gear - 1);
+
+    if(current_gear < 2 && cs.getRpm() > 8000)
+        return(current_gear + 1);
+
+    if(current_gear >= 2 && cs.getRpm() > 8500)
+        return(current_gear + 1);
+
+    if(current_gear >= 4 && cs.getRpm() < 4000)
+        return(current_gear - 1);
+
+    return current_gear;
+}
+
+float StateStraightLine::getSteering(CarState & cs) {
+    // based on Loiacono's SimpleDriver
+
+    const float
+      steerLock = 0.366519;
+    float
+      targetAngle = (cs.getAngle() - cs.getTrackPos() * 0.5) / steerLock;
+
+    // normalize steering
+    if(targetAngle < -1)
+        targetAngle = -1;
+    else if(targetAngle > 1)
+        targetAngle = 1;
+
+    return targetAngle;
+}
+
+float StateStraightLine::getDistTrackAxis(CarState & cs) {
+    return cs.getTrackPos();
+}
+
+float StateStraightLine::getSpeed(CarState & cs) {
+    return sqrt(pow(cs.getSpeedX(), 2) + pow(cs.getSpeedY(), 2));
+}
+
+CarControl StateStraightLine::execute(FsmDriver *fsmdriver) {
+    CarState& cs = fsmdriver->getCarState();
+    // static int flag = 0;
+
+    float brake = 0, clutch = 0;
+
+    float accel = speedPID.output(finalSpeed, StateStraightLine::getSpeed(cs), PID_DT);
+
+    // float steer = steeringPID.output(desiredDirection, getDistTrackAxis(cs), PID_DT);
+    float steer = StateStraightLine::getSteering(cs);
+
+    int gear = StateStraightLine::getGear(cs), focus = 0, meta = 0;
+
+    std::cout << "accel = " << accel << " gear = " << gear << " steer = " << steer << " fuel = " << cs.getFuel() << std::endl;
+
+    CarControl cc(accel, brake, gear, steer, clutch, focus, meta);
+
+    // std::cout << getSpeed(cs) << std::endl;
+    // if(getSpeed(cs) >= 297 && flag == 0)
+    // {
+    //     // t = clock() - t;
+    //     std::cout << "Velocidade Limite em = " << ((float)(clock() - t))/(CLOCKS_PER_SEC) << " segundos" << std::endl;
+    //     flag = 1;
+    // }
+
+    return cc;
+}
+
+StateStraightLine::StateStraightLine() : speedPID(KP, KI, KD) {
+    finalSpeed = FINAL_SPEED;
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+//StateOutOfTrack Class
+
+CarControl StateOutOfTrack::execute(FsmDriver *fsmdriver) {
+    /*
+     *
+     *
+     * INSERT SOUSA'S CODE HERE.
+     *
+     *
+     * */
+}
+//-----------------------------------------------------------------------------------------------------------------
