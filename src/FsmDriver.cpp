7598111189caca07462247cfd717335cc032bb4f
Bruno Macedo <120079585@aluno.unb.br>
2014-09-26 15:36:07 -0300

    Behavior of the StateCurve State defined.

--- src/FsmDriver.cpp
+++ src/FsmDriver.cpp
@@ -98,7 +98,7 @@ State* FsmDriver::transition(CarState &cs) {
         // Characteristics of a 'curve' to the FSM
         else {
             // p = new StateCurve;
-            p = new StateStraightLine;
+            p = new StateCurve;
             cout << "Curve" << endl;
             return p;
         }
@@ -115,13 +115,69 @@ State* FsmDriver::transition(CarState &cs) {
 //-----------------------------------------------------------------------------------------------------------------
 //StateCurve Class
 CarControl StateCurve::execute(FsmDriver *fsmdriver) {
-    /*
-     *
-     *
-     * INSERT CRESTANI'S CODE HERE.
-     *
-     *
-     * */
+    CarState cs = fsmdriver->getCarState();
+    int gear = 1;
+    float steer = 0;
+    int gear0[3]={-1,1,2};//This set of gear were defined by tests
+    float steer0[5]={0.1,0.6,-0.4,-0.8,-1};//This set of steer were defined by tests
+    float accel,accel0[3]={0,0.5,1};//This set of accel were defined by tests
+    float angle,angle_lim[4]={3.1415/2-1,-3.1415/2+1,0.1,-0.1};// angle and angles limits in this code
+    float track_dis,track_lim[2]={0.2,-0.2};
+    float factor[2]={1.5,1.2};//proportionate fator to steer, track_dis and angle
+
+
+    accel=accel0[2];
+    steer=steer0[0];
+    gear = gear0[1];
+
+    track_dis=cs.getTrackPos();
+    angle = cs.getAngle();
+
+    //printf("%f %f\n",angle,track_dis);
+        if(((angle<angle_lim[0])&&(angle>angle_lim[1]))){// In the case the car is in the right direction
+
+            if(((angle<angle_lim[2])&&(angle>angle_lim[3]))){
+
+                if(((track_dis>track_lim[0])||(track_dis<track_lim[1]))){ //change the steer when the car is in a turn
+                    gear =gear0[1];
+                    steer=factor[0]*steer0[2]*track_dis;
+                    accel = accel0[2];
+
+                }
+
+                else{
+                    if((steer>0.05)&&(steer<-0.05)){
+                        steer = steer-0.0001*steer/abs(steer);// Try to minimaze the steer when the condition of the track normalize(retiline) in order to maximaxe the speed
+                    }
+                    accel= accel0[2];
+                    if(gear<5){
+                        gear=gear+1;
+                    }
+
+                }
+            }
+
+            else{
+
+                steer=-factor[1]*steer0[2]*angle/abs(angle);// Adjust the steer according with the angle.
+                gear = gear0[1];
+                accel = accel0[1];
+
+
+            }
+
+        }
+
+        else{ //In the case the car is NOT in the right direction
+            accel = accel0[1];
+            steer= -steer0[3]*angle/abs(angle);
+            gear = gear0[1];
+        }
+
+    CarControl cc(accel,0,gear,steer,0);
+
+    return cc;
+
 }
 
 //-----------------------------------------------------------------------------------------------------------------
@@ -136,7 +192,7 @@ int StateStraightLine::getGear(CarState & cs) {
     if(current_gear > 1 && current_gear < 4 && cs.getRpm() < 1500)
         return(current_gear - 1);
 
-    if(current_gear < 2 && cs.getRpm() > 10000)
+    if(current_gear < 2 && cs.getRpm() > 9500)
         return(current_gear + 1);
 
     if(current_gear >= 2 && cs.getRpm() > 9500)
