4a70179c262f779cb11c30c5c486fa32058ff844
sousasmurf <g.sousasmurf@gmail.com>
2014-10-02 19:07:33 -0300

    Simple Stuck Manager

--- FSMDriver/Stuck.h
+++ FSMDriver/Stuck.h
@@ -7,12 +7,19 @@ class Stuck : public State {
 public:
     virtual CarControl execute(FsmDriver *fsmdriver) {
 	    CarState& cs = fsmdriver->getCarState();
-	    CarControl cc();
+	    CarControl cc(1, 0, -1,this->getSteer(cs), 0, 0, 0);
 	    return cc;
 	}
 
     ~Stuck(){}
     Stuck(){}
-
-
+private:
+	float getSteer(CarState & cs){
+		if(cs.getAngle()>0){
+			return -1;
+		}else{
+			return 1;
+		}
+	}
+};
 #endif // FSMDRIVER_STATE_OUTOFTRACK_H
