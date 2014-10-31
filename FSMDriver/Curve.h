29c08f5cf45d5f597805c6076e01257c3d274cb2
Crestani <matheus@PC.(none)>
2014-10-31 12:31:36 -0200

    Curve Rewritten

--- FSMDriver/Curve.h
+++ FSMDriver/Curve.h
@@ -3,6 +3,19 @@
 
 #include "FSM.h"
 
+
+int gear = 1;
+float SpeedX;
+float accel=1;
+float steer = 0,defined_steer[25]={0,0.02,0.05,0.1,0.12,0.15,0.18,0.20,0.2,0.22,0.25,0.28,0.3,0.32,0.35,0.38,0.40,0.42,0.45,0.48,0.50,0.52};// defined steer to each angle and track limiters
+float clutch=0;
+float brake=0;
+float angle,angle_limiter[10]={0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
+float track_axis_distance,track_limiter[15]={0.05,0.1,0.2,0.25,0.30,0.4,0.5,0.55,0.60,0.65,0.7,0.8,0.9,1};
+float direction_angle;
+int size_angle_limiter,size_track_limiter; 
+int contator_angle_limiter,contator_track_limiter;// contator_angle and contator_track 
+
 class FSMDriver;
 
 class Curve : public DrivingState<FSMDriver> {
@@ -18,6 +31,7 @@ private:
     void operator=(Curve const&);
 
 public:
+
     void enter(FSMDriver *driver) {
         std::cout << "Enter Curve" << std::endl;
     }
@@ -25,244 +39,60 @@ public:
     void exit(FSMDriver *driver) {
         std::cout << "Exit Curve" << std::endl;
     }
+	
+    int getgear(float steer, int gear,float SpeedX){
+		
+		if((steer>0.2)&&(gear>1)){
+			gear=gear -1;
+		}
+		else{
+			if(SpeedX < 75){
+				gear = 1;
+			}
+			else{
+				if(SpeedX < 100){
+					gear = 2;
+				}
+			}
+		}
+
+    return gear;
+    }
 
     virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs) {
-	   	
-int gear = 1;
-float accel=1,accel0[4]= {0.8,0.7,0.85,1};
-float steer = 0,steer0[20]={0,0.025,0.05,0.075,0.1,0.125,0.150,0.175,0.2,0.225,0.25,0.275,0.3,0.325,0.4,0.45,0.5,0.6,0.7,0.8};
-float clutch=0;
-float brake=0;
-float brake0[3]={0,0.1,0.5};
-float vx,vy;
-float angle,angle_lim[5]={0.1,0.2,0.3,0.4,0.5};
-float track_dis,track_lim[5]={0.1,0.2,0.3,0.4,0.5};
-float factor[6] ={1,-10,-20,-30,-5,2.0};
-float d_angle,d_track;
-gear=cs.getGear();
-vy = cs.getSpeedY();
-vx = cs.getSpeedX();
-angle = cs.getAngle();
-track_dis = cs.getTrackPos();
 
-d_angle = angle/abs(angle);
-d_track = -track_dis/abs(track_dis);
-d_track = d_angle*abs(angle)*35;
+		clutch=0;
+		brake=0;		
+		size_angle_limiter=sizeof(angle_limiter);
+		size_track_limiter=sizeof(track_limiter);
+		SpeedX = cs.getSpeedX();
+		angle = cs.getAngle();
+		track_axis_distance = cs.getTrackPos();
+		direction_angle = (angle>0?-1:+1);
 
-if(vx<1){
-	vx =1;	
-}
+		for(contator_angle_limiter=0;(contator_angle_limiter<size_angle_limiter);contator_angle_limiter++){//this loop allow to find the angle interval at the race
 
-if((angle<angle_lim[0])&&(angle>-angle_lim[0])){
-	if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-		steer = steer0[0]*d_track;
-	}
-	else{
-		if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-			steer = steer0[3]*d_track;
-		}
-		else{
-			if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-				steer = steer0[6]*d_track;
+			if(angle<abs(angle_limiter[size_angle_limiter])){
+		
+				for(contator_track_limiter=0;(contator_track_limiter<size_track_limiter);contator_track_limiter++){//this loop allow to find the track distance interval at the race
+
+					if(track_axis_distance<abs(track_limiter[contator_track_limiter])){
+						steer = direction_angle*defined_steer[contator_track_limiter+contator_angle_limiter];		
+						contator_angle_limiter = size_angle_limiter;
+						contator_track_limiter = size_track_limiter;
+					}
+				}	
 			}
-			else{
-				if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-					steer = steer0[9]*d_track;	
-				}
-				else{
-					if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-						steer = steer0[11]*d_track;					
-					}
-					else{
-						steer = steer0[15]*d_track;	
-					}	
-				}		
-			}		
-		}	
-	}
-
-}
-else{
-	if((angle<angle_lim[1])&&(angle>-angle_lim[1])){
-		if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-			steer = steer0[3]*d_track;
 		}
-		else{
-			if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-				steer = steer0[4]*d_track;
-			}
-			else{
-				if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-					steer = steer0[7]*d_track;
-				}
-				else{
-					if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-						steer = steer0[9]*d_track;
-					}
-					else{
-						if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-							steer = steer0[12]*d_track;
-						}
-							
-						else{
-							steer = steer0[16]*d_track;	
-						}	
-					}		
-				}		
-			}	
+
+		gear=getgear(steer,gear,SpeedX);
+
+		if(gear>2){// to assure a better performance the curve must be done reducing velocity using clutch and accel = 0
+			accel=0;
+			clutch = 1;				
 		}
-	}
-	else{
-		if((angle<angle_lim[2])&&(angle>-angle_lim[2])){
-				if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-					steer = steer0[5]*d_track;
-				}
-				else{
-					if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-						steer = steer0[6]*d_track;
-					}
-					else{
-						if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-							steer = steer0[7]*d_track;
-						}
-						else{
-							if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-								steer = steer0[10]*d_track;
-							}
-							else{
-								if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-									steer = steer0[13]*d_track;
-								}
-								else{
-									steer = steer0[17]*d_track;	
-								}	
-							}		
-						}		
-					}	
-				}
-		}
-		else{
-			if((angle<angle_lim[3])&&(angle>-angle_lim[3])){
-				if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-					steer = steer0[6]*d_track;
-				}
-				else{
-					if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-						steer = steer0[7]*d_track;
-					}
-					else{
-						if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-							steer = steer0[9]*d_track;
-						}
-						else{
-							if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-								steer = steer0[11]*d_track;
-							}
-							else{
-								if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-									steer = steer0[14]*d_track;
-								}
-								else{
-									steer = steer0[18]*d_track;	
-								}	
-							}		
-						}		
-					}	
-				}
-			}
-			else{
-				if((angle<angle_lim[4])&&(angle>-angle_lim[4])){
-						if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-							steer = steer0[8]*d_track;
-						}
-						else{
-							if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-								steer = steer0[9]*d_track;
-							}
-							else{
-								if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-									steer = steer0[10]*d_track;
-								}
-								else{
-									if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-										steer = steer0[12]*d_track;
-									}
-									else{
-										if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-											steer = steer0[15]*d_track;
-										}
-										else{
-											steer = steer0[19]*d_track;	
-										}	
-									}		
-								}		
-							}	
-						}
-				
-				}
-				else{
-					if((track_dis<track_lim[0])&&(track_dis>-track_lim[0])){
-						steer = steer0[9]*d_track;
-					}
-					else{
-						if((track_dis<track_lim[1])&&(track_dis>-track_lim[1])){
-							steer = steer0[10]*d_track;
-						}
-						else{
-							if((track_dis<track_lim[2])&&(track_dis>-track_lim[2])){
-								steer = steer0[11]*d_track;
-							}
-							else{
-								if((track_dis<track_lim[3])&&(track_dis>-track_lim[3])){
-									steer = steer0[12]*d_track;
-								}
-								else{
-									if((track_dis<track_lim[4])&&(track_dis>-track_lim[4])){
-										steer = steer0[14]*d_track;
-									}	
-									else{
-										steer = steer0[20]*d_track;	
-									}
-								}		
-							}		
-						}	
-					}
-							
-				}
-							
-			}
-		}	
-	}
 
-
-}
-
-if((steer>0.5)&&(gear>1)){
-	gear=gear -1;
-}
-
-if(vx < 70){
-	gear = 1;
-}
-else{
-	if(vx < 100){
-		gear = 2;
-	}
-	else{
-		if(vx <150){
-			gear = 3;
-		}
-		else{
-			gear = 4;
-		}
-	}
-
-}
-printf("\n%f\n",steer);
-clutch= factor[0]*vy;//According with the YSpeed
-
-
-CarControl cc(accel,brake,gear,steer,clutch);
+		CarControl cc(accel,brake,gear,steer,clutch);
 
 return cc;
 
