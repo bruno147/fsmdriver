5b9ce4c561a80d9683400dafae13b96861ecf164
Guilherme N. Ramos <ramos@gnramos.com>
2014-11-04 17:59:38 -0200

    Tried to clean up OutOfTrack.h code.

--- FSMDriver/OutOfTrack.h
+++ FSMDriver/OutOfTrack.h
@@ -5,6 +5,14 @@
 
 class FSMDriver;
 
+const float MAX_SKIDDING = 3;
+const float NEGATIVE_ACCEL_PERCENT = 0.1;
+const int VELOCITY_GEAR_4 = 90;
+const int VELOCITY_GEAR_3 = 70;
+const int VELOCITY_GEAR_2 = 40;
+const float MAX_RETURN_ANGLE = 0.7;
+const float MIN_RETURN_ANGLE = 0.5;
+
 class OutOfTrack : public DrivingState<FSMDriver> {
 public:
     static OutOfTrack *instance() {
@@ -13,15 +21,13 @@ public:
     }
 
 private:
-    OutOfTrack() : 
-        MAX_SKIDDING(3), NEGATIVE_ACCEL_PERCENT(0.1), 
-        VELOCITY_GEAR_4(90), VELOCITY_GEAR_3(70), 
-        VELOCITY_GEAR_2(40), MAX_RETURN_ANGLE(0.7),
-        MIN_RETURN_ANGLE(0.5) {}
+    OutOfTrack() {}
     OutOfTrack(OutOfTrack const &);
     void operator=(OutOfTrack const&);
 
 public:
+    ~OutOfTrack(){}
+
     void enter(FSMDriver *driver) {
         std::cout << "Enter OutOfTrack" << std::endl;
     }
@@ -31,52 +37,41 @@ public:
     }
 
     virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs) {
-	    CarControl cc(this->getAccel(cs), this->getBrake(cs), this->getGear(cs), this->getSteer(cs), 0, 0, 0);
-	    return cc;
-	}
-    static OutOfTrack* Instance() {
-        static OutOfTrack instance;
-        return &instance;
-    }
+        const float clutch = 0;
+        const int focus = 0, meta = 0;
 
-    ~OutOfTrack(){}
+	    return CarControl(getAccel(cs), getBrake(cs), getGear(cs), getSteer(cs), clutch, focus, meta);
+	}
 
 private:
-    float MAX_SKIDDING;
-    float NEGATIVE_ACCEL_PERCENT;
-    int VELOCITY_GEAR_4;
-    int VELOCITY_GEAR_3;
-    int VELOCITY_GEAR_2;
-    float MAX_RETURN_ANGLE;
-    float MIN_RETURN_ANGLE;
-
-    float getBrake(CarState & cs){
-        if(cs.getSpeedX()<0) return 1;
+    float getBrake(CarState &cs) {
+        if(cs.getSpeedX() < 0) return 1;
         if(abs(cs.getSpeedY()) > MAX_SKIDDING) return 0.1;
         
         return 0;
 	}
 
-    float getAccel(CarState & cs){
-        return(1-abs(cs.getSpeedY())*NEGATIVE_ACCEL_PERCENT);        //can be negative, need some fix
+    float getAccel(CarState &cs) {
+        return(1-abs(cs.getSpeedY())*NEGATIVE_ACCEL_PERCENT); /* @todo can be negative, need some fix */
     }
 
-    int getGear(CarState & cs){
-        if(cs.getSpeedX()>VELOCITY_GEAR_4) return cs.getGear(); //out of track the gear control based on velocity seems better than the one based on rpm
-                                                                //need reverse behavior 
-        if(cs.getSpeedX()>VELOCITY_GEAR_3) return 3;
-        if(cs.getSpeedX()>VELOCITY_GEAR_2) return 2;
+    int getGear(CarState &cs) {
+        if(cs.getSpeedX() > VELOCITY_GEAR_4) return cs.getGear(); //out of track the gear control based on velocity seems better than the one based on rpm
+                                                                /* @todo need reverse behavior */
+        if(cs.getSpeedX() > VELOCITY_GEAR_3) return 3;
+        if(cs.getSpeedX() > VELOCITY_GEAR_2) return 2;
         
         return 1;
 	}
 
-	float getSteer(CarState & cs){
-	    if(cs.getTrackPos()>0){                             //aim to go back to the track with a range of angles, between 40 and 28 with relation to the axis of track
-	        if(cs.getAngle()>MAX_RETURN_ANGLE) return 1;    //0.7rad is about 40 degrees
-	        if(cs.getAngle()<MIN_RETURN_ANGLE) return -1;  //0.5rad is about 28 degress, this values are just a guess
+	float getSteer(CarState &cs) {
+		float angle = cs.getAngle();
+	    if(cs.getTrackPos() > 0){ //aim to go back to the track with a range of angles, between MIN_RETURN_ANGLE and MAX_RETURN_ANGLE with relation to the axis of track
+	        if(angle > MAX_RETURN_ANGLE) return 1;
+	        if(angle < MIN_RETURN_ANGLE) return -1;
 	    } else {
-	        if(cs.getAngle()<-(MAX_RETURN_ANGLE)) return -1;
-	        if(cs.getAngle()>-(MIN_RETURN_ANGLE)) return 1;
+	        if(angle < -(MAX_RETURN_ANGLE)) return -1;
+	        if(angle > -(MIN_RETURN_ANGLE)) return 1;
 	    }
         
         return 0;
