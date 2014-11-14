6747847867d61b69b0904a9a5469e8a1e59faaf7
Bruno Macedo <120079585@aluno.unb.br>
2014-11-14 15:22:39 -0200

    Steer remade.

--- FSMDriver/Curve.h
+++ FSMDriver/Curve.h
@@ -3,18 +3,6 @@
 
 #include "FSM.h"
 
-/******************************************************************************/
-extern const int START_GEAR;
-
-const float STABLE_STEERING = 0.2;
-const float OVER_100_GEAR = 2; /* @todo isso depende do carro, não deveria ser assim. */
-const float START_GEAR_MAX_SPEED = 75; /* @todo isso depende do carro, não deveria ser assim. */
-const float SECOND_GEAR_MAX_SPEED = 100; /* @todo isso depende do carro, não deveria ser assim. */
-
-float defined_steer[25]={0,0.02,0.05,0.1,0.12,0.15,0.18,0.20,0.2,0.22,0.25,0.28,0.3,0.32,0.35,0.38,0.40,0.42,0.45,0.48,0.50,0.52};// defined steer to each angle and track limiters
-float angle_limiter[10]={0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
-float track_limiter[15]={0.05,0.1,0.2,0.25,0.30,0.4,0.5,0.55,0.60,0.65,0.7,0.8,0.9,1};
-/******************************************************************************/
 
 class FSMDriver;
 
@@ -41,70 +29,54 @@ public:
     }
 
     virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs) {
-		float steer = getSteer2(cs);
-		currentGear = getGear(steer, cs.getSpeedX());
-
+		float steer = getSteer(cs);
+		int gear = StraightLine::getGear(cs);
 		float accel  = getAccel(cs);
+        float brake = getBrake(cs);
 		float clutch = 0;
 
-		return CarControl(accel, getBrake(cs), StraightLine::getGear(cs), steer, clutch);
+		return CarControl(accel, brake, gear, steer, clutch);
 	}
 
     ~Curve(){}
 
 private:
-	inline bool shouldDecreaseGear(float steer) {
-		return (steer > STABLE_STEERING && currentGear > START_GEAR);
-	}
-
-    int getGear(float steer, float speedX) {
-		if(shouldDecreaseGear(steer))
-			return currentGear-1;
-
-		if(speedX < START_GEAR_MAX_SPEED)
-			return START_GEAR;
-
-		if(speedX < SECOND_GEAR_MAX_SPEED)
-			return START_GEAR + 1;
-
-		return currentGear;
-    }
-    float getAccel(CarState cs) {
-    	return 1-abs(cs.getAngle());
-
+    float getAccel(CarState &cs) { //@todo Change accelaration logic.
+    	return 0.6;
     }
 
     float getBrake(CarState cs) {
     	return (cs.getSpeedX() < 0 ? 1:0);
     }
 
-    float getSteer2(CarState &cs) {
-    	float angle = cs.getAngle();
-    	float pos = cs.getTrackPos();
-    	
+    float findFarthestDirection(CarState &cs) {
+        float farthestSensor = -INFINITY;
+        float farthestDirection = 0;
+        for (int i = 0; i < 19; i++) {
+            if (farthestSensor < cs.getTrack(i)) {
+                farthestSensor = cs.getTrack(i);
+                farthestDirection = i;
+            }
+        }
+        farthestDirection = -M_PI/2 + farthestDirection*M_PI/18;
+        return -farthestDirection;
+    }
 
-    	cout << "angle: " << angle-1.5*pos << endl;
-    	return angle-1.5*pos;
+    float normalizeSteer(float angle) {
+        const float MAXSTEER = 0.785398;
+        return angle/MAXSTEER;
     }
 
     float getSteer(CarState &cs) {
-		float angle = cs.getAngle();
-		bool is_positive_angle = (angle > 0);
-		
-		angle = abs(angle);
-		int a = 0; 
-		while(angle < angle_limiter[a]) ++a;
-		/* a value will *not* exceed array size because getAngle() range is 
-		within angle_limiter bounds. */
+        if (cs.getAngle() < -M_PI || cs.getAngle() > M_PI) {
+            return cs.getAngle();
+        }
+        else {
+        	float angle = findFarthestDirection(cs);   	
+        	return normalizeSteer(angle);
+        }
+    }
 
-		float track_axis_distance = abs(cs.getTrackPos());
-		int t = 0;
-		while(track_axis_distance < track_limiter[t]) ++t;
-		/* t value will *not* exceed array size because getTrackPos() range is 
-		within track_limiter bounds. */
-
-		return (is_positive_angle ? -defined_steer[a+t] : defined_steer[a+t]);
-	}
 };
 
 #endif // FSMDRIVER_STATE_CURVE_H
