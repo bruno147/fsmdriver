6807f08f78b2c01129d25898069a28bcbc79a38c
Gastd <gabriel.araujo.5000@gmail.com>
2015-01-11 18:30:51 -0200

    Split old OutOfTrack.h in OutOfTrack.h and OutOfTrack.cpp

---
+++ FSMDriver/OutOfTrack.cpp
@@ -0,0 +1,61 @@
+#include "OutOfTrack.h"
+
+OutOfTrack::OutOfTrack() {}
+
+OutOfTrack::OutOfTrack(OutOfTrack const &) {}
+
+OutOfTrack *OutOfTrack::instance() {
+    static OutOfTrack instance;
+    return &instance;
+}
+
+void OutOfTrack::enter(FSMDriver *driver) {
+    cout << "Enter OutOfTrack" << endl;
+}
+
+void OutOfTrack::exit(FSMDriver *driver) {
+    cout << "Exit OutOfTrack" << endl;
+}
+
+CarControl OutOfTrack::drive(FSMDriver *fsmdriver, CarState &cs) {
+    const float clutch = 0;
+    const int focus = 0, meta = 0;
+
+    return CarControl(getAccel(cs), getBrake(cs), getGear(cs), getSteer(cs), clutch, focus, meta);
+}
+
+float OutOfTrack::getBrake(CarState &cs) {
+    if(cs.getSpeedX() < 0) return 1;
+    if(abs(cs.getSpeedY()) > MAX_SKIDDING) return 0.1;
+    
+    return 0;
+}
+
+float OutOfTrack::getAccel(CarState &cs) {
+    return(1-abs(cs.getSpeedY())*NEGATIVE_ACCEL_PERCENT); /* @todo can be negative, need some fix */
+}
+
+int OutOfTrack::getGear(CarState &cs) {
+    if(cs.getSpeedX() > VELOCITY_GEAR_4) return cs.getGear(); //out of track the gear control based on velocity seems better than the one based on rpm
+                                                            /* @todo need reverse behavior */
+    if(cs.getSpeedX() > VELOCITY_GEAR_3) return 3;
+    if(cs.getSpeedX() > VELOCITY_GEAR_2) return 2;
+    
+    return 1;
+}
+
+float OutOfTrack::getSteer(CarState &cs) {
+	float angle = cs.getAngle();
+    if(cs.getTrackPos() > 0){ //aim to go back to the track with a range of angles, between MIN_RETURN_ANGLE and MAX_RETURN_ANGLE with relation to the axis of track
+        if(angle > MAX_RETURN_ANGLE) return 1;
+        if(angle < MIN_RETURN_ANGLE) return -1;
+    } else {
+        if(angle < -(MAX_RETURN_ANGLE)) return -1;
+        if(angle > -(MIN_RETURN_ANGLE)) return 1;
+    }
+    
+    return 0;
+}
+
+
+OutOfTrack::~OutOfTrack() {}
--- FSMDriver/OutOfTrack.h
+++ FSMDriver/OutOfTrack.h
@@ -1,8 +1,8 @@
 #ifndef FSMDRIVER_STATE_OUTOFTRACK_H
 #define FSMDRIVER_STATE_OUTOFTRACK_H
 
+#include <cmath>
 #include "FSM.h"
-
 
 
 class FSMDriver;
@@ -18,68 +18,23 @@ public:
     static float MAX_RETURN_ANGLE;
     static float MIN_RETURN_ANGLE;
     /******************************************************************************/
-    static OutOfTrack *instance() {
-        static OutOfTrack instance;
-        return &instance;
-    }
+    
+    static OutOfTrack *instance();
+    void enter(FSMDriver *driver);
+    void exit(FSMDriver *driver);
+    virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs);
+
+    ~OutOfTrack();
 
 private:
-    OutOfTrack() {}
+    OutOfTrack();
     OutOfTrack(OutOfTrack const &);
     void operator=(OutOfTrack const&);
 
-public:
-    ~OutOfTrack(){}
-
-    void enter(FSMDriver *driver) {
-        cout << "Enter OutOfTrack" << endl;
-    }
-
-    void exit(FSMDriver *driver) {
-        cout << "Exit OutOfTrack" << endl;
-    }
-
-    virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs) {
-        const float clutch = 0;
-        const int focus = 0, meta = 0;
-
-	    return CarControl(getAccel(cs), getBrake(cs), getGear(cs), getSteer(cs), clutch, focus, meta);
-	}
-
-private:
-    float getBrake(CarState &cs) {
-        if(cs.getSpeedX() < 0) return 1;
-        if(abs(cs.getSpeedY()) > MAX_SKIDDING) return 0.1;
-        
-        return 0;
-	}
-
-    float getAccel(CarState &cs) {
-        return(1-abs(cs.getSpeedY())*NEGATIVE_ACCEL_PERCENT); /* @todo can be negative, need some fix */
-    }
-
-    int getGear(CarState &cs) {
-        if(cs.getSpeedX() > VELOCITY_GEAR_4) return cs.getGear(); //out of track the gear control based on velocity seems better than the one based on rpm
-                                                                /* @todo need reverse behavior */
-        if(cs.getSpeedX() > VELOCITY_GEAR_3) return 3;
-        if(cs.getSpeedX() > VELOCITY_GEAR_2) return 2;
-        
-        return 1;
-	}
-
-	float getSteer(CarState &cs) {
-		float angle = cs.getAngle();
-	    if(cs.getTrackPos() > 0){ //aim to go back to the track with a range of angles, between MIN_RETURN_ANGLE and MAX_RETURN_ANGLE with relation to the axis of track
-	        if(angle > MAX_RETURN_ANGLE) return 1;
-	        if(angle < MIN_RETURN_ANGLE) return -1;
-	    } else {
-	        if(angle < -(MAX_RETURN_ANGLE)) return -1;
-	        if(angle > -(MIN_RETURN_ANGLE)) return 1;
-	    }
-        
-        return 0;
-	}
-
+    float getBrake(CarState &cs);
+    float getAccel(CarState &cs);
+    int getGear(CarState &cs);
+    float getSteer(CarState &cs);
 };
 
 #endif // FSMDRIVER_STATE_OUTOFTRACK_H
