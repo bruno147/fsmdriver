24997a9a1c7d07ce1afd855b7bd33eb41c308a03
sousasmurf <g.sousasmurf@gmail.com>
2015-03-06 23:22:37 -0300

    temp

--- FSMDriver Genetic Algorithm/DriverGeneticAlgorithm.cpp
+++ FSMDriver Genetic Algorithm/DriverGeneticAlgorithm.cpp
@@ -61,6 +61,7 @@ int main (int argc, char* argv[]) {
 
 			// Assigns a fitness score to every chromosome through testing in race
 			for (int i = 0; i < POPULATION_SIZE; i++) {
+				cout << endl << "i " << i << " population " << binToHex(Population[i].bits) << endl;
 				Population[i].fitness = assignFitness (Population[i].bits);
 				totalFitness += Population[i].fitness;
 			}
@@ -74,29 +75,33 @@ int main (int argc, char* argv[]) {
 			}
 
 			// @toDo Include Log Method to keep every generation archived
+			log(generationsRequired, Population, bestChromosome);
 
 			// Creates new population members through crossover and/or mutation (by chance)
 			chromosomeType	newPopulation[POPULATION_SIZE];
-			int 			populationCounter;
+			int 			populationCounter=0;
 
 			while (populationCounter < POPULATION_SIZE) {
 				// Selects 2 new members to apply crossover and mutation
 				string offspring1 = roulette (totalFitness, Population);
 				string offspring2 = roulette (totalFitness, Population);
 
+				//string offspring1 = Population[0].bits;
+				//string offspring2 = Population[1].bits;
+
 				crossover 	(offspring1, offspring2);
+
 				mutate 		(offspring1);
 				mutate 		(offspring2);
-				
+				// cout << "\nafter mutate" << endl;
+				// cout << endl << "C1: " << binToHex(offspring1) << "\tC2: " << binToHex(offspring2)<< endl;
 				// Replaces the old members for the new ones
 				newPopulation[populationCounter++] = chromosomeType (offspring1, 0.0f);
-				newPopulation[populationCounter++] = chromosomeType (offspring2, 0.0f);
+				newPopulation[populationCounter++] = chromosomeType (offspring2, 0.0f);	
+			}
 
-				
-
-				for (int i = 0; i < POPULATION_SIZE; i++) {
-					Population[i] = newPopulation[i];
-				}
+			for (int i = 0; i < POPULATION_SIZE; i++) {
+				Population[i] = newPopulation[i];
 			}
 			++generationsRequired;
 			cout << generationsRequired << endl; 
@@ -176,21 +181,21 @@ void DriverGeneticAlgorithm::setChromosome (string chromosome, string filename) 
 }
 */
 float DriverGeneticAlgorithm::assignFitness (string bits) {
-	// @toDo Define a metric to evaluate a chromosome (through running on TORCS)
+	
 	float result1, result2, result3;
 	string track1("forza");
 	string track2("cg1");
 	string track3("cs");
 
-	result1 = runTest(track1, bits);
-	result2 = runTest(track2, bits);
-	result3 = runTest(track3, bits);
+//	result1 = runTest(track1, bits);
+//	result2 = runTest(track2, bits);
+//	result3 = runTest(track3, bits);
 
 	float mean = totalMean(result1, result2, result3);	
-	cout << "mean: " << mean << endl;
-	cout << "resultado: " << result1 << endl;
+	//cout << "mean: " << mean << endl;
+	//cout << "resultado: " << result1 << endl;
 
-	return 1;
+	return RANDOM_NUMBER;
 }
 
 float DriverGeneticAlgorithm::runTest (string track1, string bits) {
@@ -203,13 +208,8 @@ float DriverGeneticAlgorithm::runTest (string track1, string bits) {
 	int myID = stoi(strID);
 	command = "torcs -r " + track + ".xml & ./FSMDriver " + bits + " " + strID; 
 	
-	//cout << command << endl << endl;
-
-
 	system("fuser -k 3001/udp"); 
 	if(system(command.c_str()) == -1)	cout << "DEU PALA" << endl;
-
-
 
 	/* Reattach the shared memory segment, at a different address. */
 	shared_memory = (char*) shmat (myID, (void*) 0x5000000, 0);
@@ -242,7 +242,8 @@ float DriverGeneticAlgorithm::totalMean (float result1, float result2, float res
 	// @toDo Define which mean to calculate using the results and the weights
 	mean = ((result1 * weight1) + (result2 * weight2) + (result3 * weight3)) / (weight1 + weight2 + weight3);
 
-	return mean;
+	if(mean == 0)	return 0;
+	else	return (1/mean);
 }
 
 // @toDo create communication to send the choromosome to a file to be read on TORCS
@@ -273,16 +274,19 @@ void DriverGeneticAlgorithm::mutate (string &bits) {
 }
 
 void DriverGeneticAlgorithm::crossover (string &offspring1, string &offspring2) {
-	if (RANDOM_NUMBER < DriverGeneticAlgorithm::CROSSOVER_RATE) {
+	//if (RANDOM_NUMBER < DriverGeneticAlgorithm::CROSSOVER_RATE) {
     // Randomic choice of the crossover point
     int crossover 	= (int) (RANDOM_NUMBER * CHROMOSOME_LENGTH);
 
+//    cout << "crossover " << crossover << endl;
     string new1		= offspring1.substr (0, crossover) + offspring2.substr (crossover, CHROMOSOME_LENGTH);
     string new2		= offspring2.substr (0, crossover) + offspring1.substr (crossover, CHROMOSOME_LENGTH);
+//    cout << "offspring1 " << binToHex(offspring1) << endl;
+//    cout << "offspring2 " << binToHex(offspring2) << endl;
 
     offspring1		= new1;
     offspring2		= new2;				  
-	}
+	//}
 }
 
 string DriverGeneticAlgorithm::roulette (int totalFitness, chromosomeType* Population) {
@@ -300,3 +304,122 @@ string DriverGeneticAlgorithm::roulette (int totalFitness, chromosomeType* Popul
 	}
 	return "";
 }	
+
+void DriverGeneticAlgorithm::log(int generation, chromosomeType population[], chromosomeType bestChromosome){
+	ofstream logFile;
+    logFile.open("log.txt", std::ios_base::app);
+    logFile << endl << endl;
+    logFile << endl << "Generation " << generation << endl;
+    logFile << endl << "Best Chromosome so far: " << setw(164) << "\tFitness:" << endl;
+    logFile << binToHex(bestChromosome.bits) << "\t" << bestChromosome.fitness << endl;
+    logFile << endl << "Population: " << endl;
+
+    std::vector<chromosomeType> sortPopulation;
+
+    for(int i = 0; i < POPULATION_SIZE; i++)	sortPopulation.push_back(population[i]);
+
+	sortPopulation = merge_sort(sortPopulation);
+    for(int i = 0; i < POPULATION_SIZE; i++){
+    	//logFile << binToHex(population[i].bits) << "\t" << population[i].fitness << endl;
+    	bitset<64> set1(sortPopulation[i].bits.substr(0,64));
+    	bitset<64> set2(sortPopulation[i].bits.substr(64,128));
+    	bitset<64> set3(sortPopulation[i].bits.substr(128,192));
+    	bitset<64> set4(sortPopulation[i].bits.substr(192,256));
+    	bitset<64> set5(sortPopulation[i].bits.substr(256,320));
+    	bitset<64> set6(sortPopulation[i].bits.substr(320,384));
+    	bitset<64> set7(sortPopulation[i].bits.substr(384,448));
+    	bitset<64> set8(sortPopulation[i].bits.substr(448,512));
+    	bitset<64> set9(sortPopulation[i].bits.substr(512,576));
+    	bitset<64> set10(sortPopulation[i].bits.substr(576,640));
+    	bitset<64> set11(sortPopulation[i].bits.substr(640,704));
+
+    	stringstream ss;
+    	string aux;
+    	for(int k = 0; k < 11; k++)
+    	{
+    		ss << hex << sortPopulation[i].bits.substr(k*64 , (k + 1)* 64);
+    	}
+    	//cout << "string " << sortPopulation[i].bits << endl;
+    	//cout << "hex " << hex << set1.to_ulong() << set2.to_ulong() << set3.to_ulong() << set4.to_ulong() << endl;
+    	cout << endl;
+    	ss << hex << set1.to_ullong();
+    	ss << hex << set2.to_ullong();
+    	ss << hex << set3.to_ullong();
+    	ss << hex << set4.to_ullong();
+    	ss << hex << set5.to_ullong();
+    	ss << hex << set6.to_ullong();
+
+    	// cout << hex << set1.to_ullong();
+    	// cout << hex << set2.to_ullong();
+    	// cout << hex << set3.to_ullong();
+    	// cout << hex << set4.to_ullong();
+    	// cout << hex << set5.to_ullong();
+    	// cout << hex << set6.to_ullong();
+
+    	cout << endl << hex << ss.str() << endl;
+
+    	logFile << hex << set1.to_ullong(); // << "  ";
+    	logFile << hex << set2.to_ullong(); // << "  ";
+    	logFile << hex << set3.to_ullong(); // << "  ";
+    	logFile << hex << set4.to_ullong(); // << "  ";
+    	logFile << hex << set5.to_ullong(); // << "  ";
+    	logFile << hex << set6.to_ullong(); // << "  ";
+    	logFile << hex << set7.to_ullong(); // << "  ";
+    	logFile << hex << set8.to_ullong(); // << "  ";
+    	logFile << hex << set9.to_ullong(); // << "  ";
+    	logFile << hex << set10.to_ullong(); // << "  ";
+    	logFile << hex << set11.to_ullong(); // << "  ";
+    	logFile << "\t    " << sortPopulation[i].fitness << endl;
+    	logFile << ss.str() << endl;
+    }
+
+    logFile << endl << endl;
+    //logFile << sortPopulation.size() << endl;
+    //logFile << f << endl;
+    logFile.close();
+}
+
+//string DriverGeneticAlgorithm::binToHex(string Chromosome){
+char* DriverGeneticAlgorithm::binToHex(string Chromosome){
+	// char* Chromosome.c_str() = Chromosome.c_str();
+	char hexstr[176];
+	char four[4];
+	int hexnum;
+
+
+	for(int i = 0;i < 176;++i)
+	{
+    	hexnum = 0;
+	 
+	    strncpy(four, Chromosome.c_str() + (i * 4), 4);
+	    for(int j = 0;j < 4;++j)	hexnum += (four[j] - '0') << (3 - j);
+	 
+	    sprintf(hexstr + i, "%X", hexnum);
+  	}
+
+  	string ret(hexstr);
+
+ 	return (char*)ret.c_str();
+//  	return ret;
+}
+
+std::vector<chromosomeType> DriverGeneticAlgorithm::merge_sort(const std::vector<chromosomeType> &data)
+{
+	if (data.size() <= 1) {
+		return data;
+	}
+ 
+	int middle = data.size() / 2;
+	std::vector<chromosomeType> left(data.begin(), data.begin()+middle);
+	std::vector<chromosomeType> right(data.begin()+middle, data.end());
+ 
+	left = merge_sort(left);
+	right = merge_sort(right);
+ 
+	std::vector<chromosomeType> result(data.size());
+	std::merge(left.begin(), left.end(), 
+	           right.begin(), right.end(),
+	           result.begin());
+ 
+	return result;
+}
--- FSMDriver/ApproachingCurve.h
+++ FSMDriver/ApproachingCurve.h
@@ -50,13 +50,13 @@ private:
         float speedFactor = 5000;                       //The target speed is obtained through a constant factor
 
         if (cs.getFocus(2) == -1) {                     //Focus sensors are available only once per second
-            cout << "FOCUS MISS!" << endl;
+            // cout << "FOCUS MISS!" << endl;
             rSensor = cs.getTrack(10);                  //Use track sensors
             cSensor = cs.getTrack(9);
             lSensor = cs.getTrack(8);
         }
         else {
-            cout << "FOCUS HIT!" << endl;
+            // cout << "FOCUS HIT!" << endl;
             rSensor = cs.getFocus(3);                   //Use focus sensors
             cSensor = cs.getFocus(2);
             lSensor = cs.getFocus(1);
