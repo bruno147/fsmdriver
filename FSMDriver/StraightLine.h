4ec3ee5d668aae146163e7de43f4420a8f6d8667
Gastd <gabriel.araujo.5000@gmail.com>
2015-03-25 12:43:31 -0300

    Split old StraightLine.h in StraightLine.h and StraightLine.cpp

--- FSMDriver/StraightLine.h
+++ FSMDriver/StraightLine.h
@@ -4,87 +4,39 @@
 #include "FSM.h"
 
 /******************************************************************************/
-int START_GEAR = 1;
-int LOW_GEAR_LIMIT = 4;
+extern int START_GEAR; // = 1;
+extern int LOW_GEAR_LIMIT; // = 4;
 
-int LOW_RPM = 1500;
-int AVERAGE_RPM = 4000;
-int HIGH_RPM = 9500;
+extern int LOW_RPM; // = 1500;
+extern int AVERAGE_RPM; // = 4000;
+extern int HIGH_RPM; // = 9500;
 /******************************************************************************/
 
 class FSMDriver;
-
 class StraightLine : public DrivingState<FSMDriver> {
 public:
-    static StraightLine *instance() {
-        static StraightLine instance;
-        return &instance;
-    }
+    static StraightLine *instance();
+    void enter(FSMDriver *driver);
+    void exit(FSMDriver *driver);
+    virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs);
+    static int getGear(CarState &cs);
+
+    ~StraightLine();
 
 private:
-    StraightLine() {}
+    StraightLine();
     StraightLine(StraightLine const &);
     void operator=(StraightLine const&);
 
-public:
-    ~StraightLine(){}
+    float getBrake(CarState &cs);
 
-    void enter(FSMDriver *driver) {
-        //cout << "Enter StraightLine" << endl;
-    }
-
-    void exit(FSMDriver *driver) {
-        //cout << "Exit StraightLine" << endl;
-    }
-
-    virtual CarControl drive(FSMDriver *fsmdriver, CarState &cs) {
-        const float accel = 1, brake = 0, clutch = 0;
-        const int focus = 0, meta = 0;
-        float steer = cs.getAngle();
-
-        return CarControl(accel, brake, getGear(cs), steer, clutch, focus, meta);
-    }
-
-private:
-    static inline bool runningOnLow(int rpm) {
-        return (rpm < LOW_RPM);
-    }
-    static inline bool runningUnderAverage(int rpm) {
-        return (rpm <= AVERAGE_RPM);
-    }
-    static inline bool runningOnHigh(int rpm) {
-        return (rpm > HIGH_RPM);
-    }
-    static inline bool isLowGear(int gear) {
-        return (gear > START_GEAR && gear < LOW_GEAR_LIMIT);
-    }
-    static inline bool isHighGear(int gear) {
-        return (gear >= LOW_GEAR_LIMIT);
-    }
-    static bool shouldDecreaseGear(int currentGear, int rpm) {
-        if(isLowGear(currentGear) && runningOnLow(rpm)) return true;
-        if(isHighGear(currentGear) && runningUnderAverage(rpm)) return true;
-        return false;
-    }
-    static inline bool shouldIncreaseGear(int currentGear, int rpm) {
-        return runningOnHigh(rpm);
-    }
-    float getBrake(CarState &cs) {
-        return (cs.getSpeedX() < 0 ? 1:0);
-    }
-
-public:
-    static int getGear(CarState &cs) {
-        int gear = cs.getGear();
-        if(gear <= 0) return START_GEAR;
-
-        int rpm = cs.getRpm();
-
-        if(shouldIncreaseGear(gear, rpm)) ++gear;
-        else if(shouldDecreaseGear(gear, rpm)) --gear;
-
-        return gear;
-    }
+    static bool shouldDecreaseGear(int currentGear, int rpm);
+    static inline bool runningOnLow(int rpm);
+    static inline bool runningUnderAverage(int rpm);
+    static inline bool runningOnHigh(int rpm);
+    static inline bool isLowGear(int gear);
+    static inline bool isHighGear(int gear);
+    static inline bool shouldIncreaseGear(int currentGear, int rpm);
 };
 
 #endif // FSMDRIVER_STATE_STRAIGHTLINE_H
